7abc766b34e613547348653ec4934dc97803d7da
inderpartap <inderpartaps.cheema2014@vit.ac.in>
2016-06-09 11:20:36 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    
+    //    Pageloader
+    $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    loading: true,
+    loadingParentElement: 'body',
+    loadingClass: 'animsition-loading',
+    loadingInner: '',
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : false,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
+    $('#page-slider').fullpage({
+        sectionSelector: 'section',
+        responsiveWidth: 600,
+        verticalCentered: true,
+        navigation: true,
+        navigationPosition: 'right',
+    });
+});
