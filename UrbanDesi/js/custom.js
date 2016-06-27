e3947765a1a2234153b92cabde1596cbf98b4f8d
inderpartap <inderpartaps.cheema2014@vit.ac.in>
2016-06-27 10:54:21 +0530

    made changes to custom.js and pants.html

--- UrbanDesi/js/custom.js
+++ UrbanDesi/js/custom.js
@@ -1,4 +1,34 @@
 function wishlisted() {
 
-$('.root-nav .btn-wishlist').toggleClass('highlight');
+    $('.root-nav .btn-wishlist').toggleClass('highlight');
 }
+
+var basket = 1;
+
+function addtobasket() {
+        $('.root-nav .btn-basket').addClass('highlight');
+        $('.root-nav .btn-basket .num').text(basket);
+        basket++;
+        $('.notification-basket').addClass('show');
+setTimeout(function () { 
+    $('.notification-basket').removeClass('show');
+}, 1000);
+}
+
+
+
+function togglefilter() {
+  //this.$container.toggleClass("filter-opened");
+        $(".template-grid").toggleClass("filter-open");
+        $(".block-btn-filter").toggleClass("hide");
+
+        $(".filters").toggleClass("show");
+        $(".filters").toggleClass("fixed");
+        $(".filters-cleaner").toggleClass("show");
+        $(".btn-clear").toggleClass("selected");
+        $(".btn-mobile-filter").toggleClass("selected");
+        
+        
+        
+}
+
